2becd2e69d9521d410adfe434b04a6553fdb5863
Lawton Spelliscy <lawton@tribalscale.com>
2018-05-16 15:08:13 -0400

    test deployment

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "helo World"
+chmod 400 brooklyn-ec2.pem
+ssh -i "brooklyn-ec2.pem" ec2-user@ec2-13-59-122-224.us-east-2.compute.amazonaws.com "cd Dice | cd bin | npm install | node www"
