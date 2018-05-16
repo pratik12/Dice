3e721dc8be49b81a985eb2dec2ab6de7ba094097
Lawton Spelliscy <lawton@tribalscale.com>
2018-05-16 16:22:53 -0400

    cahnge deploy file

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ echo $DEPLOY_KEY_PAIR
 echo $DEPLOY_KEY_PAIR | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > DeployKeyPair.pem
 #cat DeployKeyPair.pem
 chmod 400 DeployKeyPair.pem
-ssh -i DeployKeyPair.pem ec2-user@ec2-13-59-122-224.us-east-2.compute.amazonaws.com "cd Dice/bin && node www"
+ssh -o "StrictHostKeyChecking no" -i DeployKeyPair.pem ec2-user@ec2-13-59-122-224.us-east-2.compute.amazonaws.com "cd Dice/bin && node www"
