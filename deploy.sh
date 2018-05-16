3a705624576ac31b8de9a2e1de710e8f32f53a04
Lawton Spelliscy <lawton@tribalscale.com>
2018-05-16 15:55:01 -0400

    edit script files

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 echo "helo World"
+rm DeployKeyPair.pem
 touch DeployKeyPair.pem
 
 echo $DEPLOY_KEY_PAIR
-echo $DEPLOY_KEY_PAIR > DeployKeyPair.pem
-cat DeployKeyPair.pem
-#chmod 400 brooklyn-ec2.pem
+echo $DEPLOY_KEY_PAIR | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > DeployKeyPair.pem
+#cat DeployKeyPair.pem
+chmod 400 DeployKeyPair.pem
 ssh -i DeployKeyPair.pem ec2-user@ec2-13-59-122-224.us-east-2.compute.amazonaws.com "cd Dice/bin && node www"
