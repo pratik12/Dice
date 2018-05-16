56656da13c49223b025dadb79e849df0634ca4e3
Lawton Spelliscy <lawton@tribalscale.com>
2018-05-16 15:33:39 -0400

    update script files

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 echo "helo World"
-chmod 400 brooklyn-ec2.pem
-ssh -i "brooklyn-ec2.pem" ec2-user@ec2-13-59-122-224.us-east-2.compute.amazonaws.com "cd Dice/bin && node www"
+touch DeployKeyPair.pem
+
+echo $DEPLOY_KEY_PAIR
+echo $DEPLOY_KEY_PAIR > DeployKeyPair.pem
+cat DeployKeyPair.pem
+#chmod 400 brooklyn-ec2.pem
+ssh -i DeployKeyPair.pem ec2-user@ec2-13-59-122-224.us-east-2.compute.amazonaws.com "cd Dice/bin && node www"
