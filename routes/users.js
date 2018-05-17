ce9ad9f5bfd90ae942f2634e273277122e615a92
Lawton Spelliscy <lawton@tribalscale.com>
2018-05-17 14:11:31 -0400

    add a new package

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/test', async function (req, res, next) {
+  const res = await axios('https://api.ipify.org/?format=json');
+  res.send(res.data);
 });
 
 module.exports = router;
