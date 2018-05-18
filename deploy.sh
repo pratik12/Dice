7f39b899719cf9ddcd8022c3560787772b16bc31
Lawton Spelliscy <lawton@tribalscale.com>
2018-05-18 10:45:20 -0400

    Try to close ssh[C

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ touch DeployKeyPair.pem
 echo $DEPLOY_KEY_PAIR
 echo $DEPLOY_KEY_PAIR | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > DeployKeyPair.pem
 chmod 400 DeployKeyPair.pem
-ssh -o "StrictHostKeyChecking no" -i DeployKeyPair.pem ec2-user@ec2-13-59-122-224.us-east-2.compute.amazonaws.com "cd && cd Dice && pm2 stop all && git pull && cd bin && npm start"
+ssh -o "StrictHostKeyChecking no" -i DeployKeyPair.pem ec2-user@ec2-13-59-122-224.us-east-2.compute.amazonaws.com "cd && cd Dice && pm2 stop all && git pull && cd bin && npm start && exit"
 
 #aws --version
