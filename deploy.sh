a96935bd25e070015c0310c6c157ca02a478a7ec
Lawton Spelliscy <lawton@tribalscale.com>
2018-05-18 11:31:48 -0400

    test pipeline

--- deploy.sh
+++ deploy.sh
@@ -11,13 +11,11 @@ echo $DEPLOY_KEY_PAIR
 echo $DEPLOY_KEY_PAIR | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > DeployKeyPair.pem
 chmod 400 DeployKeyPair.pem
 ssh -o "StrictHostKeyChecking no" -i DeployKeyPair.pem ec2-user@ec2-13-59-122-224.us-east-2.compute.amazonaws.com << EOSSH
-cd 
 cd Dice
 pm2 stop all
+git checkout $BRANCH_NAME
 git pull
 cd bin
 npm start
 exit
 EOSSH
-
-#aws --version
