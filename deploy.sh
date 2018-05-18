a3fe30a918b6d2c95a0a52ff7b6c97fbdb07be37
Lawton Spelliscy <lawton@tribalscale.com>
2018-05-18 10:21:21 -0400

    tst pipeline

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ echo $DEPLOY_KEY_PAIR
 echo $DEPLOY_KEY_PAIR | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > DeployKeyPair.pem
 cat DeployKeyPair.pem
 chmod 400 DeployKeyPair.pem
-ssh -o "StrictHostKeyChecking no" -i DeployKeyPair.pem ec2-user@ec2-13-59-122-224.us-east-2.compute.amazonaws.com 'bash -s' < scriptfile.sh 
+ssh -o "StrictHostKeyChecking no" -i DeployKeyPair.pem ec2-user@ec2-13-59-122-224.us-east-2.compute.amazonaws.com 'bash -s' < './scriptfile.sh' 
 
 #aws --version
