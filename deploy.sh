6b3244f3433b95425919f449a233e4ae32bbe84e
Lawton Spelliscy <lawton@tribalscale.com>
2018-05-18 12:52:47 -0400

    trying to amke pipeline flexible

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,14 @@ echo "helo World"
 #     #rm DeployKeyPair.pem
 # fi
 touch DeployKeyPair.pem
-
+echo $BRANCH_NAME
 echo $DEPLOY_KEY_PAIR
 echo $DEPLOY_KEY_PAIR | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > DeployKeyPair.pem
 chmod 400 DeployKeyPair.pem
 ssh -o "StrictHostKeyChecking no" -i DeployKeyPair.pem ec2-user@ec2-13-59-122-224.us-east-2.compute.amazonaws.com << EOSSH
 cd Dice
 pm2 stop all
+git pull --all
 git checkout $BRANCH_NAME
 git pull
 cd bin
